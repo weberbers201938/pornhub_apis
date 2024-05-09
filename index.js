80890b0a8e0e208681fbea1e0e52139051eb2b96
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-05-09 09:34:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -119,26 +119,27 @@ app.post('/claude', (req, res) => {
     });
 });
 
-app.post('/api/react', (req, res) => {
-    const { link, type, cookie } = req.query;
-    axios.post("https://flikers.net/android/android_get_react.php", {
-        post_id: link,
-        react_type: type,
-        version: "v1.7"
-    }, {
-        headers: {
-            'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 12; V2134 Build/SP1A.210812.003)",
-            'Connection': "Keep-Alive",
-            'Accept-Encoding': "gzip",
-            'Content-Type': "application/json",
-            'Cookie': cookie
-        }
-    })
-        .then(dat => { res.json(dat.data); })
-        .catch(e => {
-            console.error(e);
-            res.json({ error: 'an error occurred' });
+app.post('/api/react', async (req, res) => {
+    try {
+        const { link, type, cookie } = req.query;
+        const response = await axios.post("https://flikers.net/android/android_get_react.php", {
+            post_id: link,
+            react_type: type,
+            version: "v1.7"
+        }, {
+            headers: {
+                'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 12; V2134 Build/SP1A.210812.003)",
+                'Connection': "Keep-Alive",
+                'Accept-Encoding': "gzip",
+                'Content-Type': "application/json",
+                'Cookie': cookie
+            }
         });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.json({ error: 'an error occurred' });
+    }
 });
         
 app.listen(PORT, () => {
